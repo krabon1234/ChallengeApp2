24d44ef24465f7d7b9065845370a2ee8dca528f4
Franek <gorczynski.f@gmail.com>
2023-04-11 15:26:52 +0200

    w21-dzien4

--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/*
+ Potrzebujemy elektronicznego systemu oceny naszych pracowników,
+aby odczytac wyniki poszczególnych osób, a następnie obliczyć statystyki z 
+wyników. Oceny są wprowadzane jako liczby zmiennoprzecinkowe od 0 do 100, 
+a te statystyki powinny pokazywac nam ocenę najwyższą, najniższą i średnią
+ */
+
+string imie = "Aneta";
+string sex = "K";
+int wiek = 25;
+
+
+if (sex == "K" && wiek < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (imie == "Ewa" && wiek == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == "M" && wiek < 18)
+{
+    Console.WriteLine("Niepelnoletni mezczyna");
+}
+    
+
+
