ea1be5eb210f7fe8f7bac1bcce9e39eacfbd3b97
Franek <gorczynski.f@gmail.com>
2023-04-16 18:50:17 +0200

    w21-dzien6 zadanie domowe

---
+++ ChallengeApp2/ChallengeApp2/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks.Sources;
+
+namespace ChallengeApp2
+{
+    public class Employee
+    {
+        public Employee(string name, string surname)
+        {
+            this.Name = name;
+            this.Surname = surname;
+           
+        }
+
+        public List<int> score = new List<int>();
+
+        public string Name { get; private set; }
+        public string Surname { get; private set;}
+        public int Age { get; private set;}
+
+
+        public void AddPoints(int point)
+        {
+            score.Add(point);
+        }
+
+        public int Result 
+        {
+            get
+            {
+                return score.Sum();
+            }
+        }
+    }
+
+
+
+}
--- ChallengeApp2/ChallengeApp2/Program.cs
+++ ChallengeApp2/ChallengeApp2/Program.cs
@@ -6,33 +6,81 @@ wyników. Oceny są wprowadzane jako liczby zmiennoprzecinkowe od 0 do 100,
 a te statystyki powinny pokazywac nam ocenę najwyższą, najniższą i średnią
  */
 
-int number = 289320 ;
-string numberInString = number.ToString();
-char[] letters = numberInString.ToArray();
+/*
+ 1. Stwórz klase Employee, w której przechowasz imie,
+nazwisko, wiek, oraz punkty pracownika w postaci liczb 
+całkowitych
 
-int[] sum = new int[10];
+2. Stwórz 3 pracowników i kaźdemu przydziel po 5 ocen z 
+zakresu od 1 do 10
 
-    foreach(char c in letters)
-    {
-        if (c == '0')   sum[0]++;
-        if (c == '1')   sum[1]++;
-        if (c == '2')   sum[2]++;
-        if (c == '3')   sum[3]++;
-        if (c == '4')   sum[4]++;
-        if (c == '5')   sum[5]++;
-        if (c == '6')   sum[6]++;
-        if (c == '7')   sum[7]++;
-        if (c == '8')   sum[8]++;
-        if (c == '9')   sum[9]++;
+3. Napisz program, kóry wyszuka pracownika z najwyższą 
+liczbą ocen a nastepnie wyswietli jego dane oraz wynik 
+ */
+
+
+using  ChallengeApp2;
 
 
 
-        //char ctemp =Convert.ToCha(i);
-        //if (c == ctemp) sum[i]++; 
-    }
+Employee emp1 = new Employee("Klaudia", "Iksinska");
+emp1.AddPoints(10);
+emp1.AddPoints(2);
+emp1.AddPoints(3);
+emp1.AddPoints(6);
+emp1.AddPoints(3);
 
-for(int i=0 ;i<sum.Length; i++)
+Employee emp2 = new Employee("Franek", "Nowak");
+emp2.AddPoints(5);
+emp2.AddPoints(6);
+emp2.AddPoints(6);
+emp2.AddPoints(6);
+emp2.AddPoints(6);
+
+
+Employee emp3 = new Employee("Zofia", "Kowalska");
+
+emp3.AddPoints(3);
+emp3.AddPoints(8);
+emp3.AddPoints(7);
+emp3.AddPoints(1);
+emp3.AddPoints(4);
+
+var highestGrades = emp1.Result;
+
+if (emp2.Result > highestGrades) highestGrades= emp2.Result;
+if (emp3.Result > highestGrades) highestGrades= emp3.Result;
+
+
+
+List<Employee> emps = new List<Employee>()
 {
-    Console.WriteLine(i + "=>"+ sum[i]);
+    emp1, emp2, emp3 // wpisanie do kontenera listy 
+};
+
+int highestGrade = -1;
+Employee bestEmployee = null;
+
+foreach(var emp in emps)
+{
+    if (emp.Result > highestGrade)
+    {
+        highestGrade = emp.Result;
+        bestEmployee = emp;
+    } 
 }
-    
+Console.WriteLine("Najwyzsza suma ocen nalezy do \n" );
+Console.WriteLine("Imie: " + bestEmployee.Name + "\n" +"Nazwisko:"+ bestEmployee.Surname + "\n");
+Console.WriteLine("i wynosi: " + highestGrade);
+
+
+
+
+
+
+
+
+
+
+
+
